e5982c4ec65c9fbdc1608316aad8116dbf23025b
dhkrock <dhkrock@outlook.com>
2021-11-14 10:48:37 -0500

    added more emotion text conditionals

--- index.js
+++ index.js
@@ -35,9 +35,17 @@ app.post('/checkmood', (req, res) => {
 app.post('/emotions', (req, res) => {
   let emotionText = req.body.emotionText.toLowerCase();
   if (emotionText.includes("suicidal") || emotionText.includes("depressed")) {
-    res.send("Please go get help. Here's the suicide hotlines: 323 go fuck yourself.");
-  } else if (emotionText.includes("happy") || emotionText.includes("excited")) {
-    res.send("Lot's of positive vibes going around. Hope you appreciate these joyful moments in life.")
+    res.send("Remember that you're not alone. Keep pushing on and talk to someone if ready to do so!");
+  } else if (emotionText.includes("sad") || emotionText.includes("horrible")) {
+    res.send("Good days can't exist without the bad ones. Focus on you and push through the rest of the day!")
+  } else if (emotionText.includes("happy") || emotionText.includes("excited") || emotionText.includes("delight") || emotionText.includes("joy")) {
+    res.send("There's always room for positive energy! Hope you appreciate these joyful moments in life.")
+  } else if (emotionText.includes("scared") || emotionText.includes("panic")) {
+    res.send("Remember to take deep, controlled breaths. Try to keep yourself distracted if you can until you can sort out your thoughts.")
+  } else if (emotionText.includes("caring") || emotionText.includes("love")) {
+    res.send("That's great to hear! Having someone like that around you can go a long way.")
+  } else if (emotionText.includes("guilt") || emotionText.includes("regret")) {
+    res.send("We're only human. Let's learn from our mistakes and keep moving forward.")
   } else {
     res.send("No significant data on these emotions yet.")
   }
