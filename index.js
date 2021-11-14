f694fd186438293243508e421db863cd76dfdad2
bsitenga <bsitenga@gmail.com>
2021-11-13 23:35:41 -0800

    added mood analyzer

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const Sentiment = require('sentiment')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send("Test");
+})
+
+//-5 is really bad, 0 is neutral, 5 is really good
+app.post('/checkmood', (req, res) => {
+  let userText = req.body.userText;
+  let sentiment = new Sentiment();
+  let result = sentiment.analyze(userText);
+  res.send(result.comparative);
 })
 
 app.listen(process.env.PORT || port, () => {
