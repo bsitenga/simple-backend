f9302043ca6a72546f3202e2eec6475e8223c826
bsitenga <bsitenga@gmail.com>
2021-11-14 00:16:43 -0800

    rounded number

--- index.js
+++ index.js
@@ -15,8 +15,35 @@ app.post('/checkmood', (req, res) => {
   let userText = req.body.userText;
   let sentiment = new Sentiment();
   let result = sentiment.analyze(userText);
-  let finalNum = result.comparative.toFixed(1);
-  res.send(finalNum);
+  let moodScore = result.comparative;
+  if (moodScore < -3) {
+    res.send("Horrible")
+  } else if (moodScore < -1) {
+    res.send("Not Great")
+  } else if (moodScore < 1) {
+    res.send("Neutral")
+  } else if (moodScore < 3) {
+    res.send("Pretty Good")
+  } else {
+    res.send("Awesome!")
+  }
+
+  res.send("Doing Okay"); // should never hit this
+})
+
+app.post('/moodranking', (req, res) => {
+  let moodScore = Number(req.body.moodScore);
+  if (moodScore < -3) {
+    res.send("Horrible")
+  } else if (moodScore < -1) {
+    res.send("Not Great")
+  } else if (moodScore < 1) {
+    res.send("Neutral")
+  } else if (moodScore < 3) {
+    res.send("Pretty Good")
+  } else {
+    res.send("Awesome!")
+  }
 })
 
 app.listen(process.env.PORT || port, () => {
