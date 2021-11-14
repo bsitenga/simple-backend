8f2a9eb64cd25ca72eab27751070a8ebe571c988
bsitenga <bsitenga@gmail.com>
2021-11-13 23:46:10 -0800

    fixed post

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const Sentiment = require('sentiment')
 const app = express()
 const port = 3000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send("Test");
@@ -9,10 +12,12 @@ app.get('/', (req, res) => {
 
 //-5 is really bad, 0 is neutral, 5 is really good
 app.post('/checkmood', (req, res) => {
+  console.log(req.body);
   let userText = req.body.userText;
   let sentiment = new Sentiment();
   let result = sentiment.analyze(userText);
-  res.send(result.comparative);
+  console.log(result);
+  res.send(result.comparative.toString());
 })
 
 app.listen(process.env.PORT || port, () => {
